4d530eb8be50d9d45d893c4a0a75f9aca64a47cb
kozsoy <kadirozsoy888@gmail.com>
2022-07-23 18:12:03 +0300

    first commit

---
+++ src/context/GameContext.js
@@ -0,0 +1,46 @@
+import { createContext, useState } from 'react';
+import App from '../App';
+import createInitialState from '../utils/createInitialState';
+
+export const GameContext = createContext();
+
+const GameWrapper = ({ children }) => {
+  // buton state
+  const [buttons, setButtons] = useState(createInitialState().buttons);
+  // buton kontrolu state
+  const [buttonState, setButtonState] = useState(
+    createInitialState().buttonState
+  );
+
+  // console.log('buttons', buttons);
+
+  // game over
+  const [gameOver, setGameOver] = useState(false);
+  // son hamle
+  const [lastMove, setLastMove] = useState({ row: null, col: null });
+  // kimin sirasi?
+  const [turn, setTurn] = useState('player');
+
+  return (
+    <GameContext.Provider
+      value={{
+        buttons,
+        setButtons,
+        buttonState,
+        setButtonState,
+        gameOver,
+        setGameOver,
+        lastMove,
+        setLastMove,
+        turn,
+        setTurn,
+      }}
+    >
+      
+      {children}
+    </GameContext.Provider>
+  );
+};
+export default GameWrapper;
+
+//38. satırı app ile sarmaladım 
